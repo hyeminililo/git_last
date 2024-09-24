e9c624ce3c43eb773568f0533b363aebd59c72de
hyeminililo <ng040115@gmail.com>
2024-09-25 02:51:43 +0900

    five

---
+++ src/real_last/Five.java
@@ -0,0 +1,8 @@
+package a201514199;
+
+public class Five {
+
+  public static void print() {
+    System.out.println("five");
+  }
+}
--- src/real_last/Main.java
+++ src/real_last/Main.java
@@ -1,5 +1,11 @@
 package real_last;
 
-public class Main { 
-    public static void main(String[] args) { System.out.println("one 201514199"); }
-    }
+public class Main {
+
+  public static void main(String[] args) {
+    Five.print();
+    Two.print();
+    System.out.println("one 202214052");
+    Three.print();
+  }
+}
